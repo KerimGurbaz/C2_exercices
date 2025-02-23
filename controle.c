26da57b39a4eafb9113e7d89b9bf7b8557a3cca7
Kerim <kerim.grbz.01@gmail.com>
2025-02-23 21:25:16 +0100

    Minimum de trois nombres

--- controle.c
+++ controle.c
@@ -12,13 +12,13 @@ int main() {
     }
 
     printf("Saisir n1 : ");
-    scanf("%d", &n1),
+    //scanf("%d", &n1),
 
-        printf("Saisir n2 : ");
-    scanf("%d", &n2);
+    printf("Saisir n2 : ");
+    //scanf("%d", &n2);
 
     printf("Saisir n3 : ");
-    scanf("%d", &n3);
+    //scanf("%d", &n3);
 
     if (n1 > n2 && n1 > n3) {
         printf("Le maximum est : %d.\n", n1);
@@ -27,4 +27,20 @@ int main() {
     } else if (n3 > n1 && n3 > n2) {
         printf("Le maximum est : %d.\n", n1);
     }
+
+    int nb1, nb2, nb3, min;
+    printf("entrez trois nombres separes par des espaces : ");
+    scanf("%d %d %d", &nb1, &nb2, &nb3);
+
+    if (nb1 <= nb2 && nb1 <= nb3) {
+        min = nb1;
+    } else if (nb2 <= nb1 && nb2 <= nb3) {
+        min = nb2;
+    } else {
+        min = nb3;
+    }
+
+    printf("Le minimum est : %d\n", min);
+
+    return 0;
 }
